5e37dc330b4adb58c6bb9764307741f75f0dd4bd
岩谷 <221036@std.hi-joho.ac.jp>
2025-12-09 15:41:43 +0900

    settings.pyにdaphneを追記しました。

--- Sotsuken_Portable_Project/settings.py
+++ Sotsuken_Portable_Project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': 'django_user',
 
         # パスワード
-        'PASSWORD': 'your_password',
+        'PASSWORD': '!12Sotsuken34',
 
         # ホスト名
         'HOST': '127.0.0.1',
